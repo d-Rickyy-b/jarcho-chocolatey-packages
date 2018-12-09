9f71d4ab3a4606cb239a699639bb172190497c79
Jason Newcomb <jsnewcomb@pm.me>
2018-12-09 13:53:15 -0500

    Update asstrfilter & AppVeyor

--- assfiltermod/tools/chocolateybeforemodify.ps1
+++ assfiltermod/tools/chocolateybeforemodify.ps1
@@ -1,9 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installDir   = "$(Join-Path $toolsDir x86)"
-$installDir64 = "$(Join-Path $toolsDir x64)"
+$installDir32 = "$(Join-Path $toolsDir x64)"
 
-regsvr32.exe "$(Join-Path $installDir assfiltermod.dll)" /u /s
+regsvr32.exe "$(Join-Path $toolsDir assfiltermod.dll)" /u /s
 if (Get-OSArchitectureWidth -eq 64) {
-  regsvr32.exe "$(Join-Path $installDir64 assfiltermod.dll)" /u /s
+  regsvr32.exe "$(Join-Path $installDir32 assfiltermod.dll)" /u /s
 }
