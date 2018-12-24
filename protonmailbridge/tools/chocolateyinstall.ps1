67b77c29c383ec56ce6647191930c81cf0bb004e
Jason Newcomb <jsnewcomb@pm.me>
2018-12-24 15:45:53 -0500

    Add protonmailbridge

---
+++ protonmailbridge/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'protonmailbridge'
+  fileType       = 'exe'
+  silentArgs     = '/quiet'
+  validExitCodes = @(0)
+  url            = 'https://protonmail.com/download/Bridge-Installer.exe'
+  checksum       = '3F4589757E2BC22AD59E983913D420C20CADB0CBC4F9630D64F121415D7D924F'
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
