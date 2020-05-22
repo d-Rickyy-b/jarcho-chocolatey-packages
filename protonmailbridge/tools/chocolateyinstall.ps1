6226cbf57dc60bde8a4d87b2fce79b1e609f1fff
Jason Newcomb <jsnewcomb@pm.me>
2020-05-22 09:29:32 -0400

    Update protonmailbridge to v1.2.7

--- protonmailbridge/tools/chocolateyinstall.ps1
+++ protonmailbridge/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   silentArgs     = '/quiet'
   validExitCodes = @(0)
   url            = 'https://protonmail.com/download/Bridge-Installer.exe'
-  checksum       = '79339370ed42a97b06b83e771a24d5246dbdc3e128b7ec14c5bfa196a22f12ba'
+  checksum       = '5cbdb8b0f3cfa8f5f195df0a1b75421d092783a6da3f8117c89256e079c87e86'
   checksumType   = 'sha256'
 }
 
