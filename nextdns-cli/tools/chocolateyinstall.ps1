467deb8c2dba0b0a7b57b5cd5d4bfe8717064146
Jason Newcomb <jsnewcomb@pm.me>
2020-05-24 11:59:59 -0400

    Add NextDNS CLI client

---
+++ nextdns-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32      = 'https://github.com/nextdns/nextdns/releases/download/v1.5.8/nextdns_1.5.8_windows_386.zip'
+$checksum32 = '280234c3f0437923e3af00833ed29c4c416630d37f08e70065aff0bc13d93e13'
+$url64      = 'https://github.com/nextdns/nextdns/releases/download/v1.5.8/nextdns_1.5.8_windows_amd64.zip'
+$checksum64 = '291c8bd30f86eb966eb2d565ed1d67f3ba1a0b3349db484d27b92f4b6abc4dd5'
+
+$packageArgs = @{
+  packageName    = 'nextdns-cli'
+  unzipLocation  = $toolsDir
+  fileType       = 'zip'
+  url            = $url32
+  checksumType   = 'sha256'
+  checksum       = $checksum32
+  url64bit       = $url64
+  checksumType64   = 'sha256'
+  checksum64       = $checksum64
+}
+
+Install-ChocolateyZipPackage @packageArgs
