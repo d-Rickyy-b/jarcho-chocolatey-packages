c61b1a480e9cbadd469f4768dc44cba85430199c
Jason Newcomb <jsnewcomb@pm.me>
2020-06-02 21:27:04 -0400

    Fix nextdns updater. Extract the path out of the uri before performing string operations.

--- nextdns/update.ps1
+++ nextdns/update.ps1
@@ -4,14 +4,15 @@ function global:au_GetLatest {
   $url = 'https://nextdns.io/download/windows/stable'
 
   try {
-    Invoke-WebRequest -Uri "$url" -MaximumRedirection 0 -ErrorAction Ignore
+    $response = Invoke-WebRequest -Uri "$url" -MaximumRedirection 0 -ErrorAction Ignore
   } catch {
     $response = $_.Exception.Response
   }
 
-  $file = $response.Headers.Location.SubString($response.Headers.Location.LastIndexOf('/') + 1)
-  $version = $file.substring(13, $file.LastIndexOf('.'))
-  return @{ Version = $version; URL32 = "$url" }
+  $location = $response.Headers.Location.AbsolutePath
+  $file = $location.SubString($location.LastIndexOf('/') + 1)
+  $file -match 'NextDNSSetup-(?<version>[0-9.]*)\.exe'
+  return @{ Version = $matches['version']; URL32 = $url }
 }
 
 function global:au_SearchReplace {
