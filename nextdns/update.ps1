7296e3d19cadba480702e21f76cee929f8cf737c
Jason Newcomb <jsnewcomb@pm.me>
2020-06-09 21:28:59 -0400

    Fix nextdns updater.
    Location is a string or uri depending on the powershell version.

--- nextdns/update.ps1
+++ nextdns/update.ps1
@@ -1,26 +1,33 @@
-import-module au
+Import-Module au
 
 function global:au_GetLatest {
-  $url = 'https://nextdns.io/download/windows/stable'
+    $url = 'https://nextdns.io/download/windows/stable'
 
-  try {
-    $response = Invoke-WebRequest -Uri "$url" -MaximumRedirection 0 -ErrorAction Ignore
-  } catch {
-    $response = $_.Exception.Response
-  }
+    try {
+        $response = Invoke-WebRequest -Uri "$url" -MaximumRedirection 0 -ErrorAction Ignore
+    }
+    catch {
+        $response = $_.Exception.Response
+    }
 
-  $location = $response.Headers.Location.AbsolutePath
-  $file = $location.SubString($location.LastIndexOf('/') + 1)
-  $file -match 'NextDNSSetup-(?<version>[0-9.]*)\.exe'
-  return @{ Version = $matches['version']; URL32 = $url }
+    if ($response.Headers.Location.gettype() -eq [string]) {
+        $location = $response.Headers.Location
+    }
+    else {
+        $location = $response.Headers.Location.AbsolutePath
+    }
+
+    $file = $location.SubString($location.LastIndexOf('/') + 1)
+    $file -match 'NextDNSSetup-(?<version>[0-9.]*)\.exe'
+    return @{ Version = $matches['version']; URL32 = $url }
 }
 
 function global:au_SearchReplace {
-  @{
-    "tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
     }
-  }
 }
 
 update
