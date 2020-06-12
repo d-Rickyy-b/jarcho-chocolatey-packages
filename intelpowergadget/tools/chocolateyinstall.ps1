57b33bfdd66fc40a7d8c1b38b68b55908956e710
Jason Newcomb <jsnewcomb@pm.me>
2020-06-11 23:05:34 -0400

    Update intelpowergadget to 3.5.9 + fix updater.

--- intelpowergadget/tools/chocolateyinstall.ps1
+++ intelpowergadget/tools/chocolateyinstall.ps1
@@ -1,28 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
-$tempDir = "$(Get-PackageCacheLocation)"
-$tempFile = "$(Join-Path $tempDir intelpowergadget.zip)"
-$url32      = 'https://software.intel.com/file/612178/download'
-$checksum32 = 'CB224ADB799D2029679B02201C3B517224124283D9EB4F64A1FEC2182C6DF5CE'
+$url32 = 'http://software.intel.com/content/dam/develop/external/us/en/documents/IntelPowerGadget_3.5.9.msi'
+$checksum32 = '3855df25893ffbd0e304c4a518e015cd1522e1af8e29deefd0cb290c05a2fa60'
 
-$downloadArgs = @{
-  packageName   = 'intelpowergadget'
-  fileFullPath  = $tempFile
-  url           = $url32
-  checksum      = $checksum32
-  checksumType  = 'sha256'
-}
-$unzipArgs = @{
-  fileFullPath = $tempFile
-  destination  = $tempDir
-}
 $installArgs = @{
-  packageName    = 'intelpowergadget'
-  fileType       = 'msi'
-  silentArgs     = '/quiet', 'ALLUSERS=1'
-  file           = $(Join-Path $tempDir PowerGadget.msi)
-  validExitCodes = @(0, 3010, 1641, 1603)
+    packageName    = 'intelpowergadget'
+    fileType       = 'msi'
+    softwareName   = 'Intel® Power Gadget*'
+    url            = $url32
+    checksum       = $checksum32
+    checksumType   = 'sha256'
+    silentArgs     = '/quiet /norestart ALLUSERS=1'
+    validExitCodes = @(0, 3010, 1641) #1603
 }
 
-Get-ChocolateyWebFile @downloadArgs
-Get-ChocolateyUnzip @unzipArgs
-Install-ChocolateyInstallPackage @installArgs
+Install-ChocolateyPackage @installArgs
--- intelpowergadget/update.ps1
+++ intelpowergadget/update.ps1
@@ -1,22 +1,21 @@
-import-module au
+Import-Module au
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri 'https://software.intel.com/en-us/articles/intel-power-gadget-20' -UseBasicParsing
-  $url32     = $download_page.links | ? title -match 'Power Gadget .* for Windows$' | select -First 1
-  $url32.title -match 'Gadget (?<version>.*) for'
-  return @{
-    Version = $matches['version']
-    URL32 = "https://software.intel.com$($url32.href)"
-  }
+    $download_page = Invoke-WebRequest -Uri 'https://software.intel.com/content/www/us/en/develop/articles/intel-power-gadget.html' -UseBasicParsing
+    $url32 = $download_page.links | ? href -Match 'IntelPowerGadget_(?<version>.*)\.msi' | select -First 1 -expand href
+    return @{
+        Version = $matches['version']
+        URL32   = $url32
+    }
 }
 
 function global:au_SearchReplace {
-  @{
-    "tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\`$url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-      "(?i)(^\`$checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\`$url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(?i)(^\`$checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
     }
-  }
 }
 
 update
