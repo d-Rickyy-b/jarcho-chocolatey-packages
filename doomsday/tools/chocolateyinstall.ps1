7d66bf357d7b0d701f63a63a3a5ee1d5ccc07c69
Chocolatey <chocolatey@realdimensions.net>
2021-02-05 00:44:09 +0000

    AU: 1 updated - doomsday [skip ci]

--- doomsday/tools/chocolateyinstall.ps1
+++ doomsday/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url32 = 'http://api.dengine.net/1/builds?dl=doomsday_2.3.0_x86.msi'
-$checksum32 = '0cb4539bb6e011901a49683276120a8d28217e66388b5cfdc4c1f1e5b0b520e3'
-$url64 = 'http://api.dengine.net/1/builds?dl=doomsday_2.3.0_x64.msi'
-$checksum64 = '16d4299eddde556529ae0c08cfa0dfd91199a58a02582e8466647453c7659891'
+$url32 = 'http://api.dengine.net/1/builds?dl=doomsday_2.3.1_x86.msi'
+$checksum32 = 'abe45c8433f2a565ed0ec03d4ba3b45cc420238867159c23cde964f9cc203047'
+$url64 = 'http://api.dengine.net/1/builds?dl=doomsday_2.3.1_x64.msi'
+$checksum64 = '3b46c0e4fd3e11bea20f31ee4e6adf19602660c82c57816684e00d6695ed3816'
 
 $packageArgs = @{
     packageName    = 'doomsday'
