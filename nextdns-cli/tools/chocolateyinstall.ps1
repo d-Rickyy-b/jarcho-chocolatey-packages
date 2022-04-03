93a07234b7f37fe22725cb114c1b5c3c9d868464
Chocolatey <chocolatey@realdimensions.net>
2022-04-03 01:38:18 +0000

    AU: 1 updated - nextdns-cli [skip ci]

--- nextdns-cli/tools/chocolateyinstall.ps1
+++ nextdns-cli/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://github.com/nextdns/nextdns/releases/download/v1.37.10/nextdns_1.37.10_windows_386.zip'
-$checksum32 = '79d5759f4cde5d06872204fd5a7f589f48fa0b3f280142e82c4e17da83426778'
-$url64      = 'https://github.com/nextdns/nextdns/releases/download/v1.37.10/nextdns_1.37.10_windows_amd64.zip'
-$checksum64 = 'e73338180b04686743bb3bc10eb804466768fa1e107bf151a7526cb9e2528cda'
+$url32      = 'https://github.com/nextdns/nextdns/releases/download/v1.37.11/nextdns_1.37.11_windows_386.zip'
+$checksum32 = '61e4a1ec11e97d2321c214ca716f6a1890caa3565cde1b21958eace81dcc474d'
+$url64      = 'https://github.com/nextdns/nextdns/releases/download/v1.37.11/nextdns_1.37.11_windows_amd64.zip'
+$checksum64 = 'd95b3b8c8c50a6deee6811c99787553eb3eba0e67971f92dcfd2293a8fe0e9cb'
 
 $packageArgs = @{
   packageName    = 'nextdns-cli'
