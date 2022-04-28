79fb41bd70e7fa33b87556dcdf6e0b2edbececb6
Chocolatey <chocolatey@realdimensions.net>
2022-04-28 02:22:42 +0000

    AU: 1 updated - protonmailbridge [skip ci]

--- protonmailbridge/tools/chocolateyinstall.ps1
+++ protonmailbridge/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $url32 = 'https://protonmail.com/download/bridge/Bridge-Installer.exe'
-$checksum32 = 'c4d68bca1c3307962a4c7257436cf8ff99d57737c1f5894cecf83ea3c5cb1995'
+$checksum32 = 'ddfaa8f2cbf81d8d6d4bacee53eb042eb8e068b878973d1b1b96a01022608e28'
 
 $packageArgs = @{
     packageName    = 'protonmailbridge'
