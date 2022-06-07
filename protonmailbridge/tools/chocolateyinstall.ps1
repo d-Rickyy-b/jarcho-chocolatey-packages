1819475635f9d0bf2525ca5d41e42f766866f2e5
Jason Newcomb <jsnewcomb@pm.me>
2022-06-06 21:19:02 -0400

    Update protonmailbridge lints

--- protonmailbridge/tools/chocolateyinstall.ps1
+++ protonmailbridge/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url32 = 'https://protonmail.com/download/bridge/Bridge-Installer.exe'
+$url32 = 'https://proton.me/download/bridge/Bridge-Installer.exe'
 $checksum32 = 'ddfaa8f2cbf81d8d6d4bacee53eb042eb8e068b878973d1b1b96a01022608e28'
 
 $packageArgs = @{
