83fec44a8e91f4caa119a54f46245ccd678c6b05
Chocolatey <chocolatey@realdimensions.net>
2022-08-18 01:20:53 +0000

    AU: 1 updated - imhex [skip ci]

--- imhex/tools/chocolateyinstall.ps1
+++ imhex/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://github.com/WerWolv/ImHex/releases/download/v1.21.1/imhex-1.21.1-Windows-Portable.zip'
-$checksum32 = '5b4bc568211f46bcdf7761baaddb842a19b623dff6459ca8122be41850d87c3e'
+$url32 = 'https://github.com/WerWolv/ImHex/releases/download/v1.21.2/imhex-1.21.2-Windows-Portable.zip'
+$checksum32 = '998d9d62f06c0652a5036054307aa88a1a0d16ee36329f5905bebeab10ab9fd8'
 
 $installArgs = @{
     packageName   = 'imhex'
