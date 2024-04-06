07ca7b4d4c58f8ebe9523a66627d11f0ccbf1942
Rico <d-Rickyy-b@users.noreply.github.com>
2024-04-06 14:57:08 +0200

    fix(imhex): use correct casing for imhex.exe.gui
    According to the [chocolatey docs](https://docs.chocolatey.org/en-us/create/create-packages#how-do-i-set-up-shims-for-applications-that-have-a-gui), the gui shim should have the same casing as the executable. The executable's name is imhex.exe.
    Also there is the imhex-gui.exe which actually contains the gui functionality.
    
    The imhex.exe is a CLI application that will automatically start the GUI if it is being run from the explorer. This is not possible with chocolatey so even adding this gui shim will unfortunately not help hiding the command line window when calling imhex.exe.

--- imhex/tools/chocolateyinstall.ps1
+++ imhex/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $url32 = 'https://github.com/WerWolv/ImHex/releases/download/v1.33.2/imhex-1.33.2-Windows-Portable-x86_64.zip'
 $checksum32 = '2c68672591ae84b262a9e7a4b7016e7439bff22dbdd1222b141870e7d5cf9c21'
@@ -15,4 +15,5 @@ $installArgs = @{
 Install-ChocolateyZipPackage @installArgs
 
 $installDir = $toolsDir
-New-Item "$(Join-Path $installDir 'ImHex.exe.gui')" -type file -Force | Out-Null
+New-Item "$(Join-Path $installDir 'imhex.exe.gui')" -type file -Force | Out-Null
+New-Item "$(Join-Path $installDir 'imhex-gui.exe.gui')" -type file -Force | Out-Null
